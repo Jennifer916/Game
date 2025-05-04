d80b00b2878142655bec8e5d6ba6dc95f76a5eeb
Jennifer916 <jenniferchenxil2789@nycstudents.net>
2025-05-04 03:36:56 +0000

    1st change

---
+++ Level.java
@@ -0,0 +1,16 @@
+public class Level {
+    private int points;
+    private boolean goal;
+    public boolean goalReached () {
+        return goal;
+    }
+    public int getPoints () {
+        return points;
+    }
+    public void setPoints (int p ) {
+        points = p ;
+    }
+    public boolean reachGoal() {
+        goal = true;
+    }
+}
